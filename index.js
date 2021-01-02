cb35db94899fcade3714280a6e60849e0c67b734
Ritik Taneja <rats1840@gmail.com>
2021-01-02 13:12:53 +0530

    setting up view engine - ejs

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 
 const port = 8000;
 
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port, function(err) {
     if(err) {
         console.log(`Error in running the server: ${err}`);
