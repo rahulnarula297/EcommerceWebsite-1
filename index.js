29b11eac6d2683c6eb13de99313eeb080be7dce6
Ritik Taneja <rats1840@gmail.com>
2021-01-04 16:28:18 +0530

    setting up partials and layouts

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ const app = express();
 
 const port = 8000;
 
+const expressLayouts = require('express-ejs-layouts');
+
 app.set('view engine','ejs');
 app.set('views','./views');
+
 app.use(express.static('assets'));
+
+app.use(expressLayouts);
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+app.use('/', require('./routes'));
 
 app.listen(port, function(err) {
     if(err) {
